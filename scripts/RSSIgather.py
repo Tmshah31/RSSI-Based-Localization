521d8c16ed4ad494b16b58aff44cc811910ada5e
Tirath Shah <tirath31ms@gmail.com>
2025-11-11 00:34:50 -0800

    SSID and RSSI values being recorded

--- scripts/RSSIgather.py
+++ scripts/RSSIgather.py
@@ -1,12 +1,15 @@
 from scapy.all import * 
+APs = ["ESP_Beacon_one", "ESP_Beacon_two", "ESP_Beacon_three"]
 
 def beacon_parse(packet):
-    if(packet.haslayer(Dot11Beacon) and packet.haslayer(Radiotap)):
+    if(packet.haslayer(Dot11Beacon) and packet.haslayer(RadioTap)):
         ssid = packet[Dot11Elt].info.decode('utf-8')
-        if(ssid == "ESP_Beacon_One"):
-            sig_strength = packet[Radiotap].dBm_Antsignal
+        if(ssid == APs[0]):
+            sig_strength = packet[RadioTap].dBm_AntSignal
             print(f"SSID: {ssid}\nSignal Strength: {sig_strength}")
+            exit(0)
 
 
 if __name__ == "__main__":
-    sniff(iface = "wlx00c0cab9a65f", prn = beacon_parse)
+
+    sniff(iface="wlx00c0cab9a65f", prn=beacon_parse, store=0, filter="type mgt subtype beacon")
