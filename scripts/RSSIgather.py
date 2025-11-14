727b6fa335e7f9396853fe2ce0beb6aa94e592ee
Tirath Shah <tirath31ms@gmail.com>
2025-11-13 17:36:54 -0800

    Average RSSI value logic added

--- scripts/RSSIgather.py
+++ scripts/RSSIgather.py
@@ -5,30 +5,62 @@ import keyboard
 
 
 APs = ["ESP_Beacon_one", "ESP_Beacon_two", "ESP_Beacon_three"]
-flags = [0,0,0] #[RSSI_AP1 -> RSSI_AP3]
+#flags = [0,0,0] #[RSSI_AP1 -> RSSI_AP3]
+BeaconCount = {
+    "ESP_Beacon_one" : 0 ,
+    "ESP_Beacon_two" : 0,
+    "ESP_Beacon_three" : 0}
 wlan = "wlx00c0cab9a65f"
+
+#global variables so they can be access from data write function
+X = 0
+Y = 0
+
+#store the signal strength values -> make sure to convert into linear before adding them into the list 
+#These arrays need to be cleared before the beacon_parse returns to get them ready for the next run 
+ESP_1 = []
+ESP_2 = []
+ESP_3 = []
+
+def average_RSSI(values):
+    total = sum(values)
+    avg = total/len(values)
+    values = []
+    return avg
 
 def beacon_parse(packet):
     if(packet.haslayer(Dot11Beacon) and packet.haslayer(RadioTap)):
         ssid = packet[Dot11Elt].info.decode('utf-8')
-        if(ssid == APs[0] and flags[0] == 0):
+        if(ssid == APs[0] and BeaconCount['ESP_Beacon_one'] < 5):
             sig_strength = packet[RadioTap].dBm_AntSignal
             print(f"SSID: {ssid}\nSignal Strength: {sig_strength}")
-            flags[0] = 1
-        elif(ssid == APs[1] and flags[1] == 0):
+            sig_linear = 10^(sig_strength/10)
+            ESP_1.append(sig_linear)
+            BeaconCount['ESP_Beacon_one'] += 1 
+        elif(ssid == APs[1] and BeaconCount['ESP_Beacon_two'] < 5):
             sig_strength = packet[RadioTap].dBm_AntSignal
             print(f"SSID: {ssid}\nSignal Strength: {sig_strength}")
-            flags[1] = 1
-        elif(ssid == APs[2] and flags[2] == 0):
+            sig_linear = 10^(sig_strength/10)
+            ESP_2.append(sig_linear)
+            BeaconCount['ESP_Beacon_two'] += 1
+        elif(ssid == APs[2] and BeaconCount['ESP_Beacon_three'] < 5):
             sig_strength = packet[RadioTap].dBm_AntSignal
             print(f"SSID: {ssid}\nSignal Strength: {sig_strength}")
-            flags[2] = 1 
+            sig_linear = 10^(sig_strength/10)
+            ESP_3.append(sig_linear)
+            BeaconCount['ESP_Beacon_three'] += 1
 
-        if(flags[0] == 1 and flags[1] == 1 and flags[2] == 1):
+        if(BeaconCount["ESP_Beacon_one"] == 5 and BeaconCount["ESP_Beacon_two"] == 5 and BeaconCount['ESP_Beacon_three'] == 5):
             print("Found all SSID beacons")
+
+            #clear the counts for next x,y
+            BeaconCount['ESP_Beacon_one'] = 0
+            BeaconCount['ESP_Beacon_two'] = 0
+            BeaconCount['ESP_Beacon_three'] = 0
+
             return
 
-#Start the WLAN in monitor mod 
+#Start the WLAN in monitor mode -> might be better to move this into the run.sh bash script
 def Monitor_mode():
     
     os.system(f"sudo ip link set {wlan} down")
@@ -49,8 +81,7 @@ if __name__ == "__main__":
 
     #Monitor_mode()
 
-    X = 0
-    Y = 0
+    
 
     while(True):
         key = keyboard.read_event(suppress=True)
@@ -69,5 +100,7 @@ if __name__ == "__main__":
                 print(f"X: {X}")
             if key.name == "enter":
                 print(f"Starting sniff at ({X},{Y})")    
+            if key.name == 'esc':
+                exit(0)
 
     #sniff(iface=wlan, prn=beacon_parse, store=0, filter="type mgt subtype beacon")
