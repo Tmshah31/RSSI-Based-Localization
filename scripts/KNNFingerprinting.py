7284c443bde8f25b5ed523591b637c89e58d73fe
Tirath Shah <tirath31ms@gmail.com>
2025-11-24 00:39:26 -0800

    rough puesdocode for KNN

--- scripts/KNNFingerprinting.py
+++ scripts/KNNFingerprinting.py
@@ -3,3 +3,22 @@ import collections
 import numpy as np
 import pandas as pd
 import matplotlib as plt
+
+
+#load dataset
+
+
+# classify X -> parameters
+
+
+
+# classify Y -> labels 
+
+
+#split current dataset into training and test (Record new values for test?)
+
+
+#Eculidian distance function
+
+
+#Class KNN
