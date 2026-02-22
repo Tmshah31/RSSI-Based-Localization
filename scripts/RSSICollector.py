edced0bf1c0a6156a896f95fd1dd2440fd2ceaf4
Tirath Shah <tirath31ms@gmail.com>
2026-02-21 23:13:01 -0800

    Working on a more adaptable code

---
+++ scripts/RSSICollector.py
@@ -0,0 +1,104 @@
+from scapy.all import * 
+import os
+import time
+import keyboard
+import numpy as np
+import csv
+from pathlib import Path
+from datetime import datetime
+import psutil
+
+
+class RSSI:
+
+    def __init__(self, filename, Wlan, beaconNumber):
+        self.filename = filename
+        self.X = 0
+        self.Y = 0
+        self.beaconNumber = beaconNumber
+        self.BeaconCount = {}
+        self.SigStrength = {}
+        self.Wlan = Wlan
+        self.listOfAp = []
+
+    def load_file(self):
+        with open(self.filename, 'r') as f:
+            for line in f: 
+                key = line.strip()
+
+                self.listOfAp.append(key)
+
+                if key:
+                    self.BeaconCount[key] = 0
+                    self.SigStrength[key] = []
+
+
+    def process_packet(self, packet):
+        if(packet.haslayer(Dot11Beacon) and packet.haslayer(RadioTap)):
+            bssid = packet[DOT11Beacon].addr2
+
+            if bssid in self.listOfAp:
+                sig_strength = packet[RadioTap].dBm_AntSignal
+
+                sig_linear = 10**(sig_strength/10)
+                self.SigStrength[bssid].append(sig_linear) #don't forget to clear the list  
+
+                self.BeaconCount[bssid] += 1 #don't forget to reset to zero
+
+
+    def stop_sniff(self, packet):
+        return all(count == self.beaconNumber for count in self.BeaconCount.values())
+    
+    def clear_dictionaries(self):
+
+        for value in self.SigStrength.values():
+            value.clear()
+
+        for value in self.BeaconCount:
+            self.BeaconCount[value] = 0
+
+
+
+
+
+
+def list_all_net_interfaces():
+    # Get all network interfaces
+    interfaces = psutil.net_if_addrs()
+    cards = []
+
+    print("Available Network Cards:")
+    for interface_name in interfaces:
+        cards.append(interface_name)
+
+    return cards
+
+
+
+if __name__ == "__main__":
+
+
+    cards = list_all_net_interfaces()
+
+    for i in range(len(cards)):
+        print(f"{i}:{cards[i]}")
+
+    selected = int(input("Select the card for WIFI sniff:"))
+
+    print(f"{cards[selected]} has been chosen for sniff")
+
+
+    collector = RSSI("/home/tmshah/Desktop/RSSI-Based-Localization/MAC.txt", cards[selected], 5)
+
+    collector.load_file()
+
+    print(collector.BeaconCount)
+    print(collector.SigStrength)
+
+
+
+        
+
+
+
+
