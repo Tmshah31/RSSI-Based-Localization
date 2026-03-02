db0dbf24e50f6bfae83b77685085d4e87301497d
tirath shah <tirath31ms@gmail.com>
2026-03-02 12:30:21 -0800

    ughhhhh

--- scripts/RSSICollectionSystem/RSSIsystem.py
+++ scripts/RSSICollectionSystem/RSSIsystem.py
@@ -57,7 +57,7 @@ class RSSI:
 
 
     def process_packet(self, packet):
-        if(packet.haslayer(RadioTap) and packet.haslayer(Dot11)):
+        if(packet.haslayer(RadioTap)):
             bssid = packet.getlayer(Dot11).addr2
 
             
@@ -114,7 +114,7 @@ class RSSI:
 
         #clears the dictionaries from last run
         self.clear_dictionaries()
-        sniff(iface=self.Wlan, prn=self.process_packet, store=0, timeout = 5, monitor=True)
+        sniff(iface=self.Wlan, prn=self.process_packet, store=0, timeout = 5, filter="wlan type mgt subtype beacon", monitor=True)
         
         self.average_values()
 
