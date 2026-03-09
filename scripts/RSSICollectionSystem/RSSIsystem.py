db2c9794a40f602f3f8bdf7e5ec7822f6650a9fd
Tirath Shah <tirath31ms@gmail.com>
2026-03-08 23:16:57 -0700

    testing the sniff functionaly

--- scripts/RSSICollectionSystem/RSSIsystem.py
+++ scripts/RSSICollectionSystem/RSSIsystem.py
@@ -1,4 +1,4 @@
-from scapy.all import sniff, Dot11Beacon, RadioTap, Dot11
+from scapy.all import Dot11Beacon, RadioTap, Dot11
 import os
 import time
 import keyboard
@@ -111,6 +111,8 @@ class RSSI:
         return 
     
     def start_sniff(self):
+
+        from scapy.all import sniff
 
         #clears the dictionaries from last run
         self.clear_dictionaries()
